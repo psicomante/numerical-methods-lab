0a89fc4c5cadd5a2ffd07b80423206c1a37f87d1
psicomante <psicomante@gmail.com>
2014-10-24 14:32:10 +0200

    update cavalieri-simpson

--- l4_autoquad/cavSimp.m
+++ l4_autoquad/cavSimp.m
@@ -1,5 +1,13 @@
-function [I,r] = cavSimp(f,a,b)
-% Cavalieri-Simpson con 1 intervallo
+function [Q] = cavSimp(f,a,b)
+% Approssima l'integrale di f(x) su [a,b] con Cavalieri-Simpson su un
+% intervallo.
+% f = function handle, ossia "oggetto" che si pu? valutare in un punto x(double)
+% a,b = estremi di integrazione (double)
 % r = grado di esattezza polinomiale
-I = (b-a)*(f(a)+4*f((a+b)/2)+f(b))/6;
-r = 3;
+
+m = (b+a)/2;
+%Q = (b-a)*(1/6*f(a) + 4/6*f(m) + 1/6*f(b));
+
+Q = (b-a)/6*(f(a) + 4*f(m) + f(b)); 
+% effettuando meno operazioni ci sono 
+%meno errori di approssimazione potenziali
