e5d0fb8e9440e733cd94dce1e4990f83239e4c7f
psicomante <psicomante@gmail.com>
2014-11-21 15:36:46 +0100

    update con inverso

--- l7_powermethod/powermet.m
+++ l7_powermethod/powermet.m
@@ -1,7 +1,8 @@
-function [l, v, L, iter] = powermet(A, toll, maxiter)
+function [l, v, L, iter] = powermet(A, toll, maxiter, inverso)
 % Applica il metodo delle potenze per approssimare 
 % il max autovalore di A
 % Input: matrice A, tolleranza toll, max iterazioni (def:100)
+%            inverso = true per approssimare min autovalore
 % Output: max autovalore, autovettore, sequnza approx di l
 %         L opzionale = successione degli "l" calcolati
 
@@ -15,6 +16,10 @@ function [l, v, L, iter] = powermet(A, toll, maxiter)
 % assert
 assert(size(A,1) == size(A,2));
 N = length(A);
+
+if nargin < 4
+    inverso = false;
+end
 
 % default max operation allowed
 if (nargin < 3)
@@ -40,8 +45,14 @@ err = 2*toll;
 while (err > toll && iter < maxiter)
     
     x = y / norm(y); 
-    % y -> autovettore di \lambda max
-    y = A * x;  
+    
+    if inverso
+        y = A \ x;
+    else
+        % y -> autovettore di \lambda max
+        y = A * x;  
+    end
+    
     % eigenvalue calculus:
     % l -> \lambda max
     l = x' * y; % prodotto scalare, riga per colonna
@@ -61,3 +72,8 @@ while (err > toll && iter < maxiter)
 end
 
 v = y / norm(y);
+
+if inverso
+    l = 1.0 / l;
+    L = 1.0 ./ L;
+end
